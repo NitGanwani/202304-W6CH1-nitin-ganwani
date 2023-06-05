e39723924f4c331c5c6b77534712ad98490be219
Nitin Ganwani <nitganwani@gmail.com>
2023-06-05 23:23:06 +0200

    Add function to click numbers

--- src/components/key/Key.tsx
+++ src/components/key/Key.tsx
@@ -1,13 +1,25 @@
+import { PhoneContext } from "../../context/phone.context";
+import { useContext } from "react";
+
 type PropsType = {
   item: string;
 };
 
 export function Key({ item }: PropsType) {
+  const {
+    phoneContext: { handleAddNumber, isCalling },
+  } = useContext(PhoneContext);
+
+  function handleClick() {
+    if (!isCalling) handleAddNumber(item);
+  }
   return (
     <>
       <li>
         {item !== "delete" ? (
-          <button className="key">{item}</button>
+          <button className="key" onClick={handleClick}>
+            {item}
+          </button>
         ) : (
           <button className="key big">{item}</button>
         )}
