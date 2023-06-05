b58733770b0bd47ea8ced7e7f8e8ec1f30179203
Nitin Ganwani <nitganwani@gmail.com>
2023-06-05 23:33:01 +0200

    Add handleDelete function

--- src/reducers/actions.creator.ts
+++ src/reducers/actions.creator.ts
@@ -2,12 +2,18 @@ import { actionTypes } from "./actions.types";
 
 export type PhoneAction = {
   type: string;
-  payload: string | boolean;
+  payload?: string | boolean;
 };
 
 export function addNumberAction(payload: string): PhoneAction {
   return {
     type: actionTypes.add,
     payload,
+  };
+}
+
+export function deleteNumberAction(): PhoneAction {
+  return {
+    type: actionTypes.delete,
   };
 }
